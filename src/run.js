8f5a5753e828a67c4ad22a79503c67ba62ef2a81
Jeswin Kumar <jeswinpk@agilehead.com>
2015-11-14 18:52:32 +0530

    Run with new isotropy API

--- src/run.js
+++ src/run.js
@@ -1,12 +1,6 @@
 import "babel-polyfill";
 
-import getIsotropy from "isotropy";
-import koa from "koa";
-import koaMount from "koa-mount";
-import koaStatic from "koa-static";
-import pathToRegexp from "path-to-regexp";
+import isotropy from "isotropy";
 import hitchslap from "./index";
 
-let isotropy = getIsotropy({ dir: __dirname }, { koa, koaMount, koaStatic, pathToRegexp });
-
-isotropy(hitchslap,8080).then(function() { console.log("Listening on 8080") }, function(err) { console.log(err); });
+isotropy(hitchslap, __dirname, 8080).then(function() { console.log("Listening on 8080") }, function(err) { console.log(err); });
