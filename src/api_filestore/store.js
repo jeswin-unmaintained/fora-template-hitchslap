3be12dc295be132f6819750fafb92831943c9aba
Jeswin Kumar <jeswinpk@agilehead.com>
2015-11-15 19:09:06 +0530

    Get routes to work

---
+++ src/api_filestore/store.js
@@ -0,0 +1,5 @@
+export default {
+    getAll: async function() {
+        this.body = "hello.jpg, world.png";
+    }
+}
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import api_provisioning from "./api_provisioning";
 import _static from "./static"
 
 export default {
-    ui_react: { module: ui_react, path: "/" },
-    api_graphql: { module: api_graphql, path: "/graphql" },
-    api_provisioning: { module: api_provisioning, path: "/provisioning" },
-    api_filestore: { module: api_filestore, path: "/filestore" },
-    static: { module: _static, path: "/static" }
+    ui_react: { module: ui_react, type: "ui_react", path: "/" },
+    api_graphql: { module: api_graphql, type: "api_graphql", path: "/graphql" },
+    api_provisioning: { module: api_provisioning,  type: "service", path: "/provisioning" },
+    api_filestore: { module: api_filestore,  type: "service", path: "/filestore" },
+    static: { module: _static,  type: "static", path: "/static" }
 };
